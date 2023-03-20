d7be2570169157617e32150cbc279a024692bb7f
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-20 05:32:01 +0000

    Update ZKBank.sol

--- Layer2-Solutions/ZKBank.sol
+++ Layer2-Solutions/ZKBank.sol
@@ -1,18 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+interface IERC20 {
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function allowance(address owner, address spender) external view returns (uint256);
+}
+
 contract ZKBank {
     uint256 public counter;
-    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public balances;
 
-    function deposit() public payable {
-        balances[msg.sender] += msg.value;
+    function deposit(address token, uint256 amount) public {
+        uint256 allowance = IERC20(token).allowance(msg.sender, address(this));
+        require(allowance >= amount, "Insufficient allowance");
+        require(IERC20(token).balanceOf(msg.sender) >= amount, "Insufficient balance");
+        balances[msg.sender][token] += amount;
+        require(IERC20(token).transferFrom(msg.sender, address(this), amount), "Transfer failed");
     }
 
-    function withdraw(uint256 amount) public {
-        require(balances[msg.sender] >= amount, "Insufficient balance");
-        balances[msg.sender] -= amount;
-        payable(msg.sender).transfer(amount);
+    function withdraw(address token, uint256 amount) public {
+        require(balances[msg.sender][token] >= amount, "Insufficient balance");
+        balances[msg.sender][token] -= amount;
+        require(IERC20(token).transfer(msg.sender, amount), "Transfer failed");
     }
 
     function incrementCounter() public {
